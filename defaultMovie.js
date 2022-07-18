3681ebb604a2044e7f4b5974d9db6e65a7965696
pankaj5417 <87115576+pankaj5417@users.noreply.github.com>
2022-07-18 18:48:54 +0530

    resolving some error

--- defaultMovie.js
+++ defaultMovie.js
@@ -94,38 +94,19 @@ async function defaultMovie() {
   try {
     let res1 = await fetch(
       `https://api.themoviedb.org/3/movie/popular?api_key=ac0e095d5ca7ec68c7406d38019b52fa&language=en-US`,
-      {
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-      }
+      
     );
     let res2 = await fetch(
       `https://api.themoviedb.org/3/tv/popular?api_key=ac0e095d5ca7ec68c7406d38019b52fa&language=en-US`,
-      {
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-      }
+      
     );
     let res3 = await fetch(
       `https://api.themoviedb.org/3/movie/top_rated?api_key=ac0e095d5ca7ec68c7406d38019b52fa&language=en-US`,
-      {
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-      }
+      
     );
     let res4 = await fetch(
       `https://api.themoviedb.org/3/movie/upcoming/?api_key=ac0e095d5ca7ec68c7406d38019b52fa&language=en-US`,
-      { mode: 'cors',
-      headers: {
-          'Content-Type': 'application/json',
-      }
-      }
+     
     );
 
     let data1 = await res1.json();
